81ea0b7419f8b4411fdc98629421465474a6fb9e
Leah Wasser <leah.wasser@colorado.edu>
2018-02-21 18:46:14 -0700

    renaming in setup py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration(None, parent_package, top_path)
 
-    config.add_subpackage('earthlab')
+    config.add_subpackage('earthlabpy')
 
     return config
 
