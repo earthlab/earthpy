b001ee56e424cbbad5a51dd798765d40e068272b
Max Joseph <maxwellbjoseph@gmail.com>
2018-10-12 13:39:34 -0600

    PEP 257 compliance for the docstring (feeling peppy?)

--- earthpy/tests/test_spatial.py
+++ earthpy/tests/test_spatial.py
@@ -2,7 +2,7 @@ from earthpy import spatial
 
 
 def test_extent_to_json():
-    '''Check that a polygon is generated with 5 vertices'''
+    """Check that a polygon is generated with 5 vertices."""
     json = spatial.extent_to_json(minx=0, miny=0, maxx=1, maxy=1)
     assert json.get('type') == 'Polygon'
 
