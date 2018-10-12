7d182e05300622aa073e4c25f55c3f2815995629
Max Joseph <maxwellbjoseph@gmail.com>
2018-10-12 13:31:24 -0600

    adding blank line for pep8 compliance

--- earthpy/tests/test_spatial.py
+++ earthpy/tests/test_spatial.py
@@ -1,5 +1,6 @@
 from earthpy import spatial
 
+
 def test_extent_to_json():
     '''Check that a polygon is generated with 5 vertices'''
     json = spatial.extent_to_json(minx=0, miny=0, maxx=1, maxy=1)
