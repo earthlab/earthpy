5e19a97c127e6d257474b6d195553c24c140fe96
Joseph McGlinchy <jomc9287@colorado.edu>
2018-10-24 09:48:51 -0600

    second pass at dummy_tif_writer, also added unit test and removed change to normalized_diff in spatial.py

--- earthpy/tests/test_spatial.py
+++ earthpy/tests/test_spatial.py
@@ -3,60 +3,40 @@ from osgeo import gdal
 from osgeo import osr
 import rasterio as rio
 import numpy as np
-
-# a helper function to generate a 3D array of data
-def createArray(arr_shape):
-    """ createArray(arr_shape) creates a numpy ndarray with the shape defined by the tuple arr_shape
-        arr_shape should be:
-        (rows, cols, [channels])
-        the values for each channel will be the 1-based number of that channel.
-    """
-    
-    dummy_arr = np.ones(arr_shape)
-    
-    if len(arr_shape) == 2:
-        
-        # there is only one channel, so that one channel is all 1's
-        dummy_arr = np.expand_dims(dummy_arr,-1)
-    
-    else:
-        # multiply each channel by the channel number (1-based)
-        for i in range(arr_shape[-1]):
-            dummy_arr[:,:,i] *= i+1
-    
-    return dummy_arr
+import os
 
 # a helper function to write a 3D array of data to disk as a GeoTIFF 
 # the result will have no spatial info or relevance
-def dummyTifWriter(arr, dst_filename):
-    """
-        dummy_tif_writer(arr_dst_filename) writes a tif file specified by dst_filename using the array specified
-        by arr. The shape of arr should be 3 dimensional such that the last dimension is the number of bands.
-        this type of array can be created with the createArray(arr_shape) function.
-    """
+def dummy_tif_writer(arr, destfile):
+    """Writes a tif file to a specified location using an array.
+
+    Parameters
+    ----------
+    arr : numpy ndarray 
+        The array should have 3 dimensions, arranged such that
+        a the result of arr.shape is in the form [rows, columns, channels].
+        
+    destfile : filepath string
+        The filepath for where the GeoTIFF file will be written.
+    """    
+    
 
     # make sure the array is 3 dimensional, assuming last dimension is number of bands
     # this is assured by using createArray to generate the data
     assert len(arr.shape) == 3
     
     try:
-        # define a dummy geotransform
-        geotrans = (0,1,0,0,0,-1)
+        
+        dummy_geotrans = (0,1,0,0,0,-1)
 
         # get the dimensions of the array
-        n_rows, n_cols, n_channels = arr.shape
-        n_rows, n_cols, n_channels
+        y_pixels, x_pixels, n_channels = arr.shape
 
-        x_pixels = n_cols  # number of pixels in x
-        y_pixels = n_rows  # number of pixels in y
-
-        # set the driver
+        # create the GeoTIFF file
         driver = gdal.GetDriverByName('GTiff')
+        dataset = driver.Create(dst_filename, x_pixels, y_pixels, int(n_channels), gdal.GDT_Float32)
 
-        # create the file
-        dataset = driver.Create(dst_filename, x_pixels, y_pixels, int(n_channels), gdal.GDT_Byte)
-
-        # write the bands
+        # write the bands to the GeoTIFF
         for i in range(n_channels):
             dataset.GetRasterBand(i+1).WriteArray(dummy[:,:,i])
 
@@ -65,17 +45,14 @@ def dummyTifWriter(arr, dst_filename):
         srs.ImportFromEPSG(0)
         proj = srs.ExportToWkt() 
 
-        # set the geotransform
+        # Set the spatial properties of the GeoTIFF file
         dataset.SetGeoTransform(geotrans)
-
-        # set the projection
         dataset.SetProjection(proj)
 
-        # flush the cache
         dataset.FlushCache()
 
         # remove the dataset from memory
-        dataset=None
+        dataset = None
     
         return (0, dst_filename)
     
@@ -87,3 +64,17 @@ def dummyTifWriter(arr, dst_filename):
 def test_dummy():
     """"Dummy test that will pass."""
     assert 1 == 1
+    
+def test_dummy_tif_writer():
+    """ Testing dummy_tif_writer."""
+    
+    destfile = 'dummy.tif'
+    arr = np.ones((5,5,1))
+    code,fi = dummy_tif_writer(arr, destfile)
+    
+    assert code == 0
+    assert os.path.exists(destfile) is True
+    
+    # clean up the file
+    if os.path.exists(destfile):
+        os.remove(destfile)
