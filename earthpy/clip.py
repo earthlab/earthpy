659a931defbaad6ebae206ac385ee5482850c957
Caitlin McShane <mcshanec@colorado.edu>
2018-10-25 09:13:01 -0600

    Added string documentation to clip_points and clip_line_poly

--- earthpy/clip.py
+++ earthpy/clip.py
@@ -7,7 +7,14 @@ import geopandas as gpd
 
 def clip_points(shp, clip_obj):
     '''
-    Docs Here
+    A function to clip point geomerty using geopandas. Takes an input point shapefile that will be clipped to the clip_obj.
+    Points will be extracted that intersect with the clipping object.
+
+         shp: A shapefile composed of point geometry that is to be clipped to clip_obj
+
+    clip_obj: A shapefile composed of polygon geometry that is used as the reference area for clipping
+              the point data. The clip_obj's geometry is dissolved into a single geometric feature, Points
+              that intersect with the resulting geometry are then extracted from the reference area.
     '''
 
     poly = clip_obj.geometry.unary_union
@@ -18,7 +25,19 @@ def clip_points(shp, clip_obj):
 
 def clip_line_poly(shp, clip_obj):
     '''
-    docs
+    A function to clip line and polygon data using geopandas. Takes an input shapefile that is used as the clipped
+    data, and a second shapefile that is used as the clipping object or reference areaself. A spatial index is created
+    around the 'shp' input and is then intersected with the bounding box of the clip_obj. Data within this intersection
+    is extracted from the 'shp' attribute table and the resulting subset is the output of the function.
+
+         shp: A shapefile composed of line or polygon geometry that is clipped down to the reference area provided by then
+              clip_obj
+
+    clip_obj: A shapefile composed of polygon geometry that provides the reference area for clipping the 'shp' inputself.
+              The clip_obj's geometry is dissolved into a single geometric feature and intersected with the spatial sindex
+              of the 'shp' input.
+
+
     '''
 
     # Create a single polygon object for clipping
@@ -29,7 +48,6 @@ def clip_line_poly(shp, clip_obj):
     bbox = poly.bounds
     # Get a list of id's for each road line that overlaps the bounding box and subset the data to just those lines
     sidx = list(spatial_index.intersection(bbox))
-    #shp_sub = shp[shp.index.isin(sidx)]
     shp_sub = shp.iloc[sidx]
 
     # Clip the data - with these data
