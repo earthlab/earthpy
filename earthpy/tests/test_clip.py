61db23659e0ddd3261c4f1cff165760b92b720e8
Leah Wasser <leah.wasser@colorado.edu>
2018-11-08 10:13:22 -0700

    [testing] clip_shp function tests and qa (#115)
    * Several Pep8 updates and cleanup of spatial module (#113) (#114)
    
    * adding tests
    
    * code edits to account for testing
    
    * fixed EOF error
    
    * More syntax edits
    
    * Linting
    
    * More linting
    
    * adding tests for clip poly - they are a mess
    
    * Cleanup of unit tests and adding line tests
    
    * Syntax edits
    
    * edits via max's review
    
    * one more comment issue fixed
    
    * a few assertions are incorrect - fixed
    
    * ANOTHER error fixed
    
    * removing clip stuff from test_spatial
    
    * small syntax change
    
    * Fixing a suite of tests
    
    * one more test fix - please don't break please
    
    * almost forgot to test the output is gdf
    
    * Minor tweaks to formatting

--- earthpy/tests/test_clip.py
+++ earthpy/tests/test_clip.py
@@ -1,3 +1,5 @@
+"""Tests for the clip module."""
+
 import numpy as np
 import pytest
 from shapely.geometry import Polygon, Point, LineString
@@ -6,7 +8,8 @@ import geopandas as gpd
 import earthpy.clip as cl
 
 
-# Create points GDF
+""" Setup fake data for tests """
+
 pts = np.array([[2, 2],
                 [3, 4],
                 [9, 8],
@@ -24,7 +27,7 @@ locs_gdf = gpd.GeoDataFrame(locs,
 """ Create Line Objects For Testing """
 
 linea = LineString([(1, 1), (2, 2), (3, 2), (5, 3)])
-lineb = LineString([(3, 4), (5, 7), (12, 2), (10, 5), (17, 17)])
+lineb = LineString([(3, 4), (5, 7), (12, 2), (10, 5), (9, 7.5)])
 
 linez_gdf = gpd.GeoDataFrame([1, 2],
                              geometry=[linea, lineb],
@@ -35,8 +38,8 @@ linez_gdf = gpd.GeoDataFrame([1, 2],
 # Create crop box
 poly_inters = Polygon([(0, 0), (0, 10), (10, 10), (10, 0), (0, 0)])
 poly_in_gdf = gpd.GeoDataFrame([1],
-                             geometry=[poly_inters],
-                             crs={'init': 'epsg:4326'})
+                               geometry=[poly_inters],
+                               crs={'init': 'epsg:4326'})
 
 # Shift by 20
 poly_out_gdf = poly_in_gdf.copy()
@@ -52,12 +55,19 @@ donut_geom = gpd.overlay(locs_buff, poly_in_gdf,
 
 multi_poly = donut_geom.unary_union
 multi_gdf = gpd.GeoDataFrame(gpd.GeoSeries(multi_poly),
-            crs={'init': 'epsg:4326'})
+                             crs={'init': 'epsg:4326'})
 multi_gdf = multi_gdf.rename(columns={0: 'geometry'}).\
     set_geometry('geometry')
 
 
+
 """ Run clip shape tests """
+
+
+def test_returns_gdf():
+    """Test that function returns a GeoDataFrame (or GDF-like) object."""
+    out = cl.clip_shp(locs_gdf, poly_in_gdf)
+    assert hasattr(out, 'geometry')
 
 
 def test_non_overlapping_geoms():
@@ -66,10 +76,12 @@ def test_non_overlapping_geoms():
         cl.clip_shp(locs_gdf, poly_out_gdf)
 
 
-def returns_gdf():
-    """Test that function is provided 2 GDFs """
+def check_input_gdfs():
+    """Test that function fails if not provided with 2 GDFs."""
     with pytest.raises(AssertionError):
         cl.clip_shp(locs, poly_in_gdf)
+    with pytest.raises(AssertionError):
+        cl.clip_shp(poly_in_gdf, locs)
 
 
 def test_clip_points():
@@ -81,27 +93,26 @@ def test_clip_points():
 def test_clip_poly():
     """Test clipping a polygon GDF with a generic polygon geometry."""
     clipped_poly = cl.clip_shp(locs_buff, poly_in_gdf)
-    assert len(clipped_poly.geometry) == 3 and \
-           clipped_poly.geom_type[1] == "Polygon"
+    assert len(clipped_poly.geometry) == 3
+    assert clipped_poly.geom_type[1] == "Polygon"
+
 
 # TODO same test for points and lines
+
+
 def test_clip_multipoly():
-    """Test that multi poly returns a value error"""
+    """Test that multi poly returns a value error."""
     with pytest.raises(ValueError):
         cl.clip_shp(poly_in_gdf, multi_gdf)
 
 
 def test_clip_donut_poly():
-    """Test what happens with a clip when a donut hole topology is used to
-    clip points"""
-
-    clipz = cl.clip_shp(locs_gdf, donut_geom)
-    assert len(clipz.geometry) == 1 and clipz.geom_type[
-        1] == "Point"
+    """Donut holes are multipolygons, and should raise ValueErrors."""
+    with pytest.raises(ValueError):
+        cl.clip_shp(locs_gdf, donut_geom)
 
 
 def test_clip_lines():
-    """Test what happens when you give the clip_extent a line GDF"""
+    """Test what happens when you give the clip_extent a line GDF."""
     clip_line = cl.clip_shp(linez_gdf, poly_in_gdf)
-    assert len(clip_line.geometry) == 2 and clip_line.geom_type[
-        1] == "LineString"
+    assert len(clip_line.geometry) == 2
