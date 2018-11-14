293db09edae39a843cc0ef8343465df38ec83faa
Joseph McGlinchy <jomc9287@colorado.edu>
2018-11-14 10:14:36 -0700

    added 2 tests to hopefully get full coverage of mask.py

--- earthpy/tests/test_mask.py
+++ earthpy/tests/test_mask.py
@@ -28,16 +28,26 @@ def test_masked_arr_returned():
     masked = make_apply_mask(im, im_mask, [0,4])
     assert np.ma.is_masked(masked)
 
-# def test_val_list_provided:
-#     """Tuple works just fine. what about some other object type? like? """
-#
-#     with pytest.raises(AttributeError):
-#         make_apply_mask(im,
-#                     mask_arr=im_mask,
-#                     vals=[0,4])
+def test_val_list_provided():
+    """Tuple works just fine. what about some other object type? like?
+        how about a singular value? if not that, maybe a dictionary?"""
+
+    with pytest.raises(AttributeError):
+        make_apply_mask(im,
+                    mask_arr=im_mask,
+                    vals=0)
 
 # Test that a mask is provided to apply mask that doesn't have 1's in it.
 # should return - there's nothing to mask here!
+def test_mask_provided_with_no_ones():
+
+    im_mask_no_ones = im_mask*0
+    with pytest.raises(ValueError):
+        make_apply_mask(im, im_mask_no_ones, [3,4])
 
 # test that a user provides an array with a mask already. does it merge properly?
 # and return a masked array with the values expected?
+def test_im_with_mask_as_masked_array():
+    
+    im_with_mask = np.ma.masked_where(im_mask < 2, im)
+    make_apply_mask(im_with_mask, mask_arr=im_mask, vals=[0, 4])
