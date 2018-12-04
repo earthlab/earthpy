9395346e1a4c363885c386f643f9a4368e27ed9f
Joseph McGlinchy <jomc9287@colorado.edu>
2018-12-04 09:06:53 -0700

    commented out problem statement, but there's still a bug in the last assertion group

--- earthpy/tests/test_spatial.py
+++ earthpy/tests/test_spatial.py
@@ -67,10 +67,10 @@ def test_bytescale_high_low_val():
         
     # Valid case. should also take care of if statements for cmin/cmax
     val_arr = es.bytescale(arr, high=255, low=0)
-        
+    
     assert val_arr.min() == 0
     assert val_arr.max() == 255
-    
+          
     # Test scale value max is less than min
     with pytest.raises(ValueError):
         es.bytescale(arr, cmin=100, cmax=50)
@@ -82,6 +82,7 @@ def test_bytescale_high_low_val():
         
     # Test scale value max is less equal to min
     scale_arr = es.bytescale(arr, cmin=10, cmax=240)
+    
     assert scale_arr.min() == 0
     assert scale_arr.max() == 255
 
