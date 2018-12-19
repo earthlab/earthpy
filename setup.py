6244e1ae5d6af73b49cefd9aca81f90181e21401
Max Joseph <maxwellbjoseph@gmail.com>
2018-12-19 16:14:07 -0700

    Revert back to good formatting

--- setup.py
+++ setup.py
@@ -26,7 +26,9 @@ if __name__ == "__main__":
     setup(
         configuration=configuration,
         name=DISTNAME,
-        maintainer=MAINTAINER,include_package_data=True,maintainer_email=MAINTAINER_EMAIL,
+        maintainer=MAINTAINER,
+        include_package_data=True,
+        maintainer_email=MAINTAINER_EMAIL,
         description=DESCRIPTION,
         version=VERSION,
         install_requires=[
