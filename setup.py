a818618357147c1fcbeb324c1fd12b10727f2ffa
Leah Wasser <leah.wasser@colorado.edu>
2018-12-21 18:25:05 -0700

    spelling package name correct

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
             "matplotlib",
             "rasterio",
             "download",
-            "skimage",
+            "scikit-image",
         ],
         zip_safe=False,  # the package can run out of an .egg file
         classifiers=[
