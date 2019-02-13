ce826430931e5dfa0bf99278158bdc08eca98af5
Leah Wasser <leah.wasser@colorado.edu>
2019-02-13 14:34:20 -0700

    bump version to 0.6.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DISTNAME = "earthpy"
 DESCRIPTION = "A set of helper functions to make working with spatial data in open source tools easier. This package is maintained by Earth Lab and was originally designed to support the earth analytics education program."
 MAINTAINER = "Leah Wasser"
 MAINTAINER_EMAIL = "leah.wasser@colorado.edu"
-VERSION = "VERSION='0.5.1'"
+VERSION = "VERSION='0.6.0'"
 
 
 def configuration(parent_package="", top_path=None):
