3ae84b7badb702a0e0d30b51f80c8dfb7ff60622
Max Joseph <maxwellbjoseph@gmail.com>
2019-02-18 11:20:08 -0800

    Use setuptools in setup.py (#231)
    * Use distutils from the standard library in setup.py
    
    * Use setuptools instead of distutils, following PSF recommendations: https://packaging.python.org/guides/tool-recommendations/
    
    * Black formatting (oops)

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-import os
-import setuptools
-from numpy.distutils.core import setup
+from os import path
+from setuptools import setup
 
 
 DISTNAME = "earthpy"
@@ -10,21 +9,7 @@ MAINTAINER_EMAIL = "leah.wasser@colorado.edu"
 VERSION = "VERSION='0.6.0'"
 
 
-def configuration(parent_package="", top_path=None):
-    if os.path.exists("MANIFEST"):
-        os.remove("MANIFEST")
-
-    from numpy.distutils.misc_util import Configuration
-
-    config = Configuration(None, parent_package, top_path)
-    config.add_subpackage("earthpy")
-
-    return config
-
-
 # read the contents of your README file
-from os import path
-
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
@@ -32,7 +17,6 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 if __name__ == "__main__":
     setup(
-        configuration=configuration,
         name=DISTNAME,
         maintainer=MAINTAINER,
         include_package_data=True,
@@ -41,6 +25,7 @@ if __name__ == "__main__":
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
         version=VERSION,
+        packages=["earthpy"],
         install_requires=[
             "tqdm",
             "pandas",
