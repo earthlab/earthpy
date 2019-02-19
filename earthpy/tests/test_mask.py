5bc49cbf7b22c0def936a4fb5aedb825ec28bb6e
Leah Wasser <leah.wasser@colorado.edu>
2019-02-19 09:12:55 -0700

    Updates to mask  (#237)
    * small syntax change in example
    * code to test for see if massed array passed
    * added rasterio order to parameter label
    * fixed if vals statement in main funct
    * fixed two tests and _create_mask
    * removing additional comments
    * one text name update
    * updating docstring
    * Update earthpy/mask.py
    * test cleanup
    * Further simplifications to test arrays
    * Use slightly more complicated fixtures
    * Test that the right elements get masked

--- earthpy/tests/test_mask.py
+++ earthpy/tests/test_mask.py
@@ -1,52 +1,96 @@
 """ Tests for the mask module. """
 
 import numpy as np
-from earthpy.mask import make_apply_mask
+from earthpy.mask import mask_pixels
 import pytest
 
 
-im = np.random.randint(10, size=(4, 5))
-im_mask = np.random.randint(5, size=(4, 5))
+@pytest.fixture
+def im():
+    """Create image arr for masking"""
+    arr = np.array([[0, 1, 2], [3, 4, 5]])
+    return arr
 
 
-def test_arr_provided():
-    """ Test for arrays provided as image and/or mask input. """
-
-    an_arr_tup = (2, 3)
-    with pytest.raises(AttributeError):
-        make_apply_mask(an_arr_tup, mask_arr=im_mask, vals=[0, 4])
-    with pytest.raises(AttributeError):
-        make_apply_mask(im, mask_arr=an_arr_tup, vals=[0, 4])
+@pytest.fixture
+def im_mask():
+    """Create image mask"""
+    arr = np.array([[0, 0, 1], [1, 2, 2]])
+    return arr
 
 
-def test_masked_arr_returned():
+def test_arr_provided(im, im_mask):
+    """ Test that inputs are numpy arrays. """
+
+    with pytest.raises(
+        AttributeError, match="Input arr should be a numpy array."
+    ):
+        mask_pixels((2, 3), mask_arr=im_mask, vals=[0, 4])
+    with pytest.raises(
+        AttributeError, match="Input arr should be a numpy array."
+    ):
+        mask_pixels(im, mask_arr=(2, 3), vals=[0, 4])
+
+
+def test_masked_arr_returned(im, im_mask):
     """ Test for return of masked_array type. """
 
-    masked = make_apply_mask(im, im_mask, [0, 4])
+    masked = mask_pixels(im, im_mask, vals=[0])
     assert np.ma.is_masked(masked)
 
 
-def test_val_list_provided():
-    """ Provide a singular value to the vals keyword to raise AttributeError"""
+def test_vals_as_list(im, im_mask):
+    """ Test for return of masked_array type. """
 
-    with pytest.raises(AttributeError):
-        make_apply_mask(im, mask_arr=im_mask, vals=0)
-
-
-def test_mask_provided_with_no_ones():
-    """ Test that a mask is provided to apply mask that doesn't have 1's in it.
-        should return - there's nothing to mask here!"""
-
-    im_mask_no_ones = im_mask * 0
-    with pytest.raises(ValueError):
-        make_apply_mask(im, im_mask_no_ones, [3, 4])
+    with pytest.raises(
+        AttributeError, match="Values should be provided as a list"
+    ):
+        mask_pixels(im, mask_arr=im_mask, vals=(0, 1, 2, 3))
 
 
-def test_im_with_mask_as_masked_array():
-    """Test that a user provides an array with a mask already.
-       Does it merge properly?
-       and return a masked array with the values expected?"""
+def test_mask_contains_ones(im, im_mask):
+    """A boolean mask without the value 1 fails gracefully."""
 
-    im_with_mask = np.ma.masked_where(im_mask < 2, im)
-    im_result = make_apply_mask(im_with_mask, mask_arr=im_mask, vals=[0, 4])
-    assert np.ma.is_masked(im_result)
+    zero_array = im_mask * 0
+    with pytest.raises(ValueError, match="Mask requires values of 1"):
+        mask_pixels(im, mask_arr=zero_array)
+
+
+def test_mask_vals_in_arr(im, im_mask):
+    """If vals are not found in mask_arr, fail gracefully"""
+    values_not_in_mask = [99, -99]
+    with pytest.raises(
+        ValueError, match="The values provided for the mask do not occur"
+    ):
+        mask_pixels(im, im_mask, values_not_in_mask)
+
+
+def test_masked_arr_provided(im, im_mask):
+    """If a masked array is provided, mask returns masked array."""
+
+    masked_input = np.ma.masked_where(im_mask == 0, im)
+    out = mask_pixels(masked_input, im_mask, vals=[1])
+    expected_in_mask = np.array([[True, True, False], [False, False, False]])
+    expected_out_mask = np.array([[True, True, True], [True, False, False]])
+
+    assert np.array_equal(expected_in_mask, masked_input.mask)
+    assert np.array_equal(expected_out_mask, out.mask)
+
+
+def test_boolean_mask(im, im_mask):
+    """If boolean mask provided without vals, a masked arr is returned."""
+
+    boolean_mask = im_mask > 1
+    out = mask_pixels(im, boolean_mask)
+    expected_out_mask = np.array([[False, False, False], [False, True, True]])
+    assert np.array_equal(expected_out_mask, out.mask)
+
+
+def test_user_mask_arr(im, im_mask):
+    """Test that vals are provided for non boolean mask."""
+
+    with pytest.raises(
+        ValueError,
+        match="""You have provided a mask_array with no values to mask""",
+    ):
+        mask_pixels(im, im_mask)
