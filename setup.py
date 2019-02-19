bb8d9881f1027d631b09bac56e698fbb2e7bce67
Leah Wasser <leah.wasser@colorado.edu>
2019-02-18 19:17:44 -0700

    Bump version: 0.6.2 → 0.6.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
-        version="0.6.2",
+        version="'0.6.3'",
         packages=["earthpy"],
         install_requires=[
             "tqdm",
