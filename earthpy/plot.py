8aa56166971a542f7b6d7ba11411c8bd386e1c97
Leah Wasser <leah.wasser@colorado.edu>
2019-03-11 09:46:46 -0600

    fixing typo and simplifying doc test

--- earthpy/plot.py
+++ earthpy/plot.py
@@ -408,15 +408,14 @@ def make_col_list(unique_vals, nclasses=None, cmap=None):
     >>> import numpy as np
     >>> import earthpy.plot as ep
     >>> import matplotlib.pyplot as plt
-    >>> bins = [-np.inf, 2, 7, np.inf]
-    >>> im_arr_bin = np.digitize(np.random.randint(10, size=(15, 15)), bins)
+    >>> arr = np.array([[1, 2], [3, 4], [5, 4], [5, 5]])
     >>> f, ax = plt.subplots()
-    >>> im = ax.imshow(im_arr_bin, cmap="Blues")
+    >>> im = ax.imshow(arr, cmap="Blues")
     >>> the_legend = ep.draw_legend(im_ax=im)
-    # Get the array and cmap from axis object
+    >>> # Get the array and cmap from axis object
     >>> cmap_name = im.axes.get_images()[0].get_cmap().name
     >>> unique_vals = list(np.unique(im.get_array().data))
-    >>> image_colors = ep.make_col_list(unique_vals, cmap=cmap_name)
+    >>> cmap_colors = ep.make_col_list(unique_vals, cmap=cmap_name)
 
     """
     if not nclasses:
