6c8391a298b2191a388a6f163c2ab61604f0ed70
Max Joseph <maxwellbjoseph@gmail.com>
2019-03-14 16:02:12 -0600

    Minor spacing fix

--- earthpy/plot.py
+++ earthpy/plot.py
@@ -479,7 +479,7 @@ def draw_legend(im_ax, bbox=(1.05, 1), titles=None, cmap=None, classes=None):
     except AttributeError:
         raise AttributeError(
             "The legend function requires a matplotlib axis object to "
-            " run properly. You have provided a {}.".format(type(im_ax))
+            "run properly. You have provided a {}.".format(type(im_ax))
         )
 
     # If classes not provided, get them from the im array in the ax object
