6e747fc7eb5ffe821701b8a4407d3f759a726f0b
Leah Wasser <leah.wasser@colorado.edu>
2019-03-18 09:33:27 -0600

    Simplifying test

--- earthpy/tests/test_epsg.py
+++ earthpy/tests/test_epsg.py
@@ -4,7 +4,4 @@ import earthpy as et
 def test_epsg():
     """Unit test for loading EPSG to Proj4 string dictionary."""
 
-    epsg = et.epsg
-    wgs84_proj4 = epsg["4326"]
-
-    assert wgs84_proj4 == "+proj=longlat +datum=WGS84 +no_defs"
+    assert et.epsg["4326"] == "+proj=longlat +datum=WGS84 +no_defs"
