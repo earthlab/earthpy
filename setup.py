ee2aa73e5401ba2f9a6b360c9601e0c83863de71
Leah Wasser <leah.wasser@colorado.edu>
2019-03-27 16:30:42 -0600

    Bump version: 0.6.4 → 0.6.5

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
-        version="0.6.4",
+        version="0.6.5",
         packages=["earthpy"],
         install_requires=[
             "geopandas",
