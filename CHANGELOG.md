fbedbde819466951befdecd67f10a1f9a716a860
Leah Wasser <leah.wasser@colorado.edu>
2019-03-27 15:41:24 -0600

    black

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+Fix bug in `bytescale()` - ensure math to calc range is floating point vals (@lwasser #xxx)
 
 
 ## [0.6.5] - 2019-03-37
--- earthpy/tests/test_bytescale.py
+++ earthpy/tests/test_bytescale.py
@@ -8,8 +8,22 @@ def byte_arr():
     return np.random.randint(300, size=(10, 10))
 
 
+def test_high_val_range():
+    """A 16 bit int arr with values at the end of the range, should be scaled properly.
+
+    Note that this test explicitly hits the user case of someone providing an array of a
+    dtype at the end of it's range of values. Bytescale should not fail in this case.
+    """
+
+    rgb_bands = np.array([1, 32767, 3, -32768]).astype("int16")
+    arr = es.bytescale(rgb_bands)
+
+    assert arr.min() == 0
+    assert arr.max() == 255
+
+
 def test_high_val_range(byte_arr):
-    """Bad high value """
+    """A high value >255 should fail gracefully. """
 
     with pytest.raises(
         ValueError, match="`high` should be less than or equal to 255."
@@ -18,50 +32,58 @@ def test_high_val_range(byte_arr):
 
 
 def test_low_val_range(byte_arr):
-    """Bad high value """
+    """A low value <0 should fail gracefully. """
 
     # Bad low value
     with pytest.raises(
         ValueError, match="`low` should be greater than or equal to 0."
     ):
-        es.bytescale(arr, low=-100)
+        es.bytescale(byte_arr, low=-100)
 
 
 def test_high_lessthan_low(byte_arr):
-    """Bad high value """
+    """Fail gracefully when the high value is lower than the low value."""
 
     # High value is less than low value
     with pytest.raises(
         ValueError, match="`high` should be greater than or equal to `low`."
     ):
-        es.bytescale(arr, high=100, low=150)
+        es.bytescale(byte_arr, high=100, low=150)
 
 
-def test_bytescale_high_low_val():
-    """"Unit tests for earthpy.spatial.bytescale """
-    arr = np.random.randint(300, size=(10, 10))
+def test_low_high_vals_work(byte_arr):
+    """A proper high and low value argument should return an arr with thos vals as the min and max."""
 
     # Valid case. should also take care of if statements for cmin/cmax
-    val_arr = es.bytescale(arr, high=255, low=0)
+    val_arr = es.bytescale(byte_arr, high=255, low=0)
 
     assert val_arr.min() == 0
     assert val_arr.max() == 255
 
-    # Test scale value max is less than min
+
+def test_cmax_higher_than_cmin(byte_arr):
+    """Fail gracefully when the cmax is smaller than the cmin."""
+
     with pytest.raises(
         ValueError, match="`cmax` should be larger than `cmin`."
     ):
-        es.bytescale(arr, cmin=100, cmax=50)
+        es.bytescale(byte_arr, cmin=100, cmax=50)
 
-    # Test scale value max is less equal to min. Commented out for now because it breaks stuff somehow.
+
+def test_cmax_equals_cmin(byte_arr):
+    """Fail gracefully when the cmax is smaller than the cmin."""
+
     with pytest.raises(
         ValueError,
         match="`cmax` and `cmin` should not be the same value. Please specify `cmax` > `cmin`",
     ):
-        es.bytescale(arr, cmin=100, cmax=100)
+        es.bytescale(byte_arr, cmin=100, cmax=100)
 
-    # Test scale value max is less equal to min
-    scale_arr = es.bytescale(arr, cmin=10, cmax=240)
+
+def test_cmax_cmin_work(byte_arr):
+    """"Cmax and min values should yield and arr with the same min and max values."""
+
+    scale_arr = es.bytescale(byte_arr, cmin=10, cmax=240)
 
     assert scale_arr.min() == 0
     assert scale_arr.max() == 255
