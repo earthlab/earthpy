5a903209db6611e92e8406b820293019d072ec7d
Leah Wasser <leah.wasser@colorado.edu>
2019-04-03 17:24:52 -0600

    black

--- earthpy/tests/test_extent_to_json.py
+++ earthpy/tests/test_extent_to_json.py
@@ -1,4 +1,4 @@
-""" Tests for the extent to json function """
+""" Tests for the extent_to_json function """
 
 
 import pandas as pd
@@ -8,18 +8,29 @@ from shapely.geometry import Polygon, Point
 import earthpy.spatial as es
 
 
+@pytest.fixture
+def list_out():
+
+    return es.extent_to_json([0, 0, 1, 1])
+
+
 def test_extent_to_json():
-    """"Unit tests for extent_to_json()."""
-    # Giving a list [minx, miny, maxx, maxy] makes a polygon
+    """" Giving a list [minx, miny, maxx, maxy] makes a polygon"""
     list_out = es.extent_to_json([0, 0, 1, 1])
     assert list_out["type"] == "Polygon"
 
-    # The polygon is the unit square
+
+def test_extent_to_json(list_out):
+    """The polygon is the unit square"""
+
     list_poly = Polygon(list_out["coordinates"][0])
     assert list_poly.area == 1
     assert list_poly.length == 4
 
-    # Providing a GeoDataFrame creates identical output
+
+def test_gdf_format_works(list_out):
+    """ Providing a GeoDataFrame creates returns expected vals"""
+
     points_df = pd.DataFrame({"lat": [0, 1], "lon": [0, 1]})
     points_df["coords"] = list(zip(points_df.lon, points_df.lat))
     points_df["coords"] = points_df["coords"].apply(Point)
@@ -27,11 +38,17 @@ def test_extent_to_json():
     gdf_out = es.extent_to_json(gdf)
     assert gdf_out == list_out
 
-    # Giving non-list or GeoDataFrame input raises a ValueError
+
+def test_not_a_list():
+    """Providing a non-list or GeoDataFrame input raises a ValueError"""
+
     with pytest.raises(ValueError):
         es.extent_to_json({"a": "dict"})
 
-    # Giving minima that exceed maxima raises an error for both x and y coords
+
+def test_min_exceeds_max():
+    """Giving minima that exceed maxima raises error for both x and y coords"""
+
     with pytest.raises(AssertionError):
         es.extent_to_json([1, 0, 0, 1])
 
