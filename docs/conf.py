bd3013140ac58ca0d47632f3170aeca067f89d38
Leah Wasser <leah.wasser@colorado.edu>
2019-04-03 16:47:39 -0600

    Bump version: 0.6.6 → 0.6.7

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = "2019, Earth Lab"
 author = "Earth Lab"
 
 # The short X.Y version
-version = "0.6.6"
+version = "0.6.7"
 # The full version, including alpha/beta/rc tags
-release = "0.6.6"
+release = "0.6.7"
 
 
 # -- General configuration ---------------------------------------------------
