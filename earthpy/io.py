92d21c9512b9bedebf49c021a2118ccfc35dc193
Leah Wasser <leah.wasser@colorado.edu>
2020-03-25 10:31:43 -0600

    fix twitter-flood data key

--- earthpy/io.py
+++ earthpy/io.py
@@ -80,11 +80,13 @@ DATA_URLS = {
         ".",
         "zip",
     ),
-    "twitter-flood": (
-        "https://ndownloader.figshare.com/files/10960175",
-        ".",
-        "zip",
-    ),
+    "twitter-flood": [
+        (
+            "https://ndownloader.figshare.com/files/10960175",
+            "boulder_flood_geolocated_tweets.json",
+            "file",
+        )
+    ],
 }
 
 HOME = op.join(op.expanduser("~"))
