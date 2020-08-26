1d59a9a75fe902d422e4679bfc1f439e4858e5ee
nkorinek <nako1890@colorado.edu>
2020-08-26 13:21:36 -0600

    new approach

--- earthpy/plot.py
+++ earthpy/plot.py
@@ -213,10 +213,7 @@ def plot_bands(
         ...     ep.plot_bands(src.read(),
         ...                   title=titles,
         ...                   figsize=(8, 3))
-        array([<AxesSubplot:title={'center':'Red'}>,
-               <AxesSubplot:title={'center':'Green'}>,
-               <AxesSubplot:title={'center':'Blue'}>], dtype=object)
-
+        array([<AxesSubplot:title={'center':'Red'}>...
 
     """
     show = False
