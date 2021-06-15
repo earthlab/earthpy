6c5fe05044e43f1192b57af7bf8d922890163e08
nkorinek <nako1890@colorado.edu>
2021-06-15 14:54:39 -0600

    added changes requested to crs pr

--- earthpy/spatial.py
+++ earthpy/spatial.py
@@ -633,7 +633,7 @@ def crs_check(path):
     Parameters
     ----------
     path : string
-        Path to the raster file.
+        Path to a raster file in a format that rasterio can read.
 
     Returns
     -------
@@ -649,10 +649,16 @@ def crs_check(path):
                 for data in src.subdatasets:
                     with rio.open(data) as data_src:
                         crs = data_src.crs
-        return crs
+        if crs is None:
+            raise ValueError("No CRS found in data. The raster may not have one.")
+        else:
+            return crs
     except rio.errors.RasterioIOError as e:
-        print("Please only input files that can be read as a raster.\n")
-        raise rio.errors.RasterioIOError(e.__str__())
+        raise rio.errors.RasterioIOError("Oops, your data are not in a format "
+              "that rasterio can read. Please check the rasterio documentation "
+              "for accepted file formats and make sure that your data are in "
+              "raster format..\n"
+              )
 
 
 # @deprecate
