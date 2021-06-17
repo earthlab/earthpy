817d6d42be5202b4d29db997e581546bb442838b
Nathan Korinek <nako1890@colorado.edu>
2021-06-17 15:31:37 -0600

    Plot rgb with nan (#693)
    * Update sphinx_rtd_theme from 0.5.1 to 0.5.2
    
    * Update sphinx_gallery from 0.8.2 to 0.9.0
    
    * changes to plot nan values earthpy
    
    * added a test
    
    * small formatting
    
    * Update pytest from 6.2.3 to 6.2.4
    
    * Update tox from 3.23.0 to 3.23.1
    
    * Update sphinx from 3.5.3 to 4.0.2
    
    * Update pre-commit from 2.11.1 to 2.13.0
    
    * Update pytest-cov from 2.11.1 to 2.12.1
    
    * Added very basic Check CRS function.
    
    * Update importlib-metadata from 3.10.0 to 4.5.0 (#706)
    
    Co-authored-by: pyup-bot <github-bot@pyup.io>

--- earthpy/tests/test_plot_rgb.py
+++ earthpy/tests/test_plot_rgb.py
@@ -132,6 +132,22 @@ def test_stretch_image(rgb_image):
     plt.close()
 
 
+def test_stretch_image_nan(rgb_image):
+    """Test that running stretch actually stretches the data
+    to a max value of 255 and min value of 0 when nan values
+    are present within the plot_rgb fun."""
+
+    im, _ = rgb_image
+    np.place(im, im > 150, [0])
+    im = np.where(im < 25, np.nan, im)
+
+    ax = plot_rgb(im, stretch=True)
+    max_val = ax.get_images()[0].get_array().max()
+    min_val = ax.get_images()[0].get_array().min()
+    assert max_val == 255 and min_val == 0
+    plt.close()
+
+
 def test_masked_im(rgb_image):
     """Test that a masked image will be plotted using an alpha channel.
     Thus it should return an array that has a 4th dimension representing
