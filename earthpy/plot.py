f2f6fc29d1180615d2754499185514537c16c079
nkorinek <nathan.korinek@colorado.edu>
2023-06-14 14:42:19 -0400

    fixed issue with docs bild

--- earthpy/plot.py
+++ earthpy/plot.py
@@ -213,7 +213,7 @@ def plot_bands(
         ...     ep.plot_bands(src.read(),
         ...                   title=titles,
         ...                   figsize=(8, 3))
-        array([<AxesSubplot:title={'center':'Red'}>...
+        array([<Axes: title={'center': 'Red'}>...
     """
     show = False
     try:
@@ -389,7 +389,7 @@ def plot_rgb(
         ...     img_array = src.read()
         >>> # Ensure the input array doesn't have nodata values like -9999
         >>> ep.plot_rgb(img_array)
-        <AxesSubplot:>
+        <Axes: >
 
     """
 
