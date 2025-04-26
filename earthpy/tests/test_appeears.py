34f0be579bd6cd4144080d5467c27a67ffaacfff
Elsa Culler <eculler@gmail.com>
2025-04-26 16:37:07 -0600

    Add deleted status scenario

--- earthpy/tests/test_appeears.py
+++ earthpy/tests/test_appeears.py
@@ -19,7 +19,8 @@ def test_download_data():
 	)
 	gdf = gpd.read_file(quotes_url)
 	downloader = etapi.AppeearsDownloader(
-		'MOD13Q1.061', '_250m_16_days_NDVI', '01-01-2021', '01-20-2021', gdf)
+		'MOD13Q1.061', '_250m_16_days_NDVI', '01-01-2021', '01-20-2021', gdf,
+		download_key='earthpy-test')
 	downloader.download_files()
 	
 def test_download_recurring_data():
@@ -37,5 +38,6 @@ def test_download_recurring_data():
 		end_date='01-20', 
 		recurring=True,
 		year_range=[2021, 2022],
-		polygon=gdf)
+		polygon=gdf,
+		download_key='earthpy-test-recurring')
 	downloader.download_files()
